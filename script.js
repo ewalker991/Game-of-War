7584d206aa645b05801adfc2b582e1174f71030d
Erika Sanchez <erika.walker.business@gmail.com>
2021-02-15 15:51:06 -0500

    Started, basic card and game classes added. suit, rank, value arrays made. Nothing important in here yet.

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Script is linked!");
+
+suit = ["Hearts", "Spades", "Diamonds", "Clubs"];
+rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "King", "Queen", "Ace"];
+value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+class Cards {
+    constructor () {
+
+    }
+}
+
+class Game {
+    constructor () {
+
+    }
+}
